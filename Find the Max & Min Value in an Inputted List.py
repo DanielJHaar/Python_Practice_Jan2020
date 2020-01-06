9af70b9af7c68b9db285f486288991e27723a063
Daniel Joseph Haar <danieljhaar@gmail.com>
2020-01-06 06:41:38 -0800

    Add files via upload

---
+++ Find the Max & Min Value in an Inputted List.py
@@ -0,0 +1,31 @@
+largest = None
+smallest = None
+#Make an empty list before the loop starts
+numlist = list()
+#User input function, that creates a list of inputted values
+while True :
+	inp = input('Enter a number: ')
+	if inp == 'done' :
+		break
+	try:
+		fval = int(inp)
+		numlist.append(fval)
+	except:
+		print('Invalid input')
+		continue
+
+# find largest value in list
+for value in numlist :
+	if largest is None :
+		largest = value
+	elif value > largest :
+		largest = value
+
+# find smallest value in list
+for value in numlist :
+	if smallest is None :
+		smallest = value
+	elif value < smallest :
+		smallest = value
+print("Maximum is", largest)
+print("Minimum is", smallest)
