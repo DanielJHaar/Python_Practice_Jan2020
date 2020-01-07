6fa3c28644472975d0bad562ea4294ff35fe63d1
Daniel Joseph Haar <danieljhaar@gmail.com>
2020-01-07 06:12:31 -0800

    Add files via upload

---
+++ Basic Webcrawler.py
@@ -0,0 +1,14 @@
+#Basic webcrawler that retrieves all the anchor tags from a web page
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+
+#Python requests to connec to webpage, and BeautifulSoup cleans up HTML code for
+#more accurate parsing
+url = input('Enter - ')
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))
