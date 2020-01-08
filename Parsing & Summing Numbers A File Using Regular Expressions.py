7e024d16835d279d63dc17ffe27faf3be9875d72
Daniel Joseph Haar <danieljhaar@gmail.com>
2020-01-08 06:56:40 -0800

    Add files via upload

---
+++ Parsing & Summing Numbers A File Using Regular Expressions.py
@@ -0,0 +1,9 @@
+import re
+name = input('Enter filename:')
+file = open(name)
+sum = 0
+for line in file:
+    numbers = re.findall('([0-9]+)', line)
+    for number in numbers:
+        sum = sum + int(number)
+print(sum)
