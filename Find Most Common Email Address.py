25842b19f54de7fa2e760686c6c41fb75bb74208
Daniel Joseph Haar <danieljhaar@gmail.com>
2020-01-10 07:01:18 -0800

    Update Find Most Common Email Address.py

--- Find Most Common Email Address.py
+++ Find Most Common Email Address.py
@@ -10,13 +10,14 @@ for line in handle:
     # Guardian in a compound statement, the first condition must be met before continuing
     if len(words) < 3 or words[0] != 'From' :
         continue
-
+    #for each word identified in each line.  If not seen before, assign to list with value of 1.  Otherwise add 1 to count.
     for word in words:
         if word not in counts:
             counts[word] = 1
         else:
             counts[word] = counts[word] + 1
 
+#Parses through list of words and filters for those with the '@' symbol
 bigcount = None
 bigword = None
 for word,count in counts.items():
