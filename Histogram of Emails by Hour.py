f8d3e061a6ac7e1c910bb511b8a9867a16f4ea58
Daniel Joseph Haar <danieljhaar@gmail.com>
2020-01-13 20:42:57 -0800

    Add files via upload
    This python code searches through an email database saved as a .txt file, and creates a histogram of emails received by hour of day.

---
+++ Histogram of Emails by Hour.py
@@ -0,0 +1,18 @@
+name = input("Enter name of file:")
+handle = open(name)
+counts = dict()
+lst = list()
+for line in handle:
+    words = line.split()
+    if len(words) < 3 or words[0] != "From" :
+            continue
+    time = words[5].split(':')
+    hours = time[0]
+    lst.append(hours)
+
+for hour in lst:
+    counts[hour] = counts.get(hour, 0) + 1
+
+t = sorted(counts.items())
+for k, v in sorted(counts.items()) :
+    print(k, v)
